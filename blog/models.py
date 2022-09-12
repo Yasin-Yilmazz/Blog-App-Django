487d0d32db3f3da3fa70f975b5fd080dcd8c20bf
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-09-12 15:19:43 +0300

    models added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+    
+
+class Post(models.Model):
+    # d databasede görülme şekli diğeri kullanıcı menüdeki görünümü
+    OPTIONS = (
+        ("d", "Draft"),
+        ("p","Published")
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category,on_delete=models.PROTECT)
+    publisy_date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=OPTIONS, default="d")
+    slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return self.title
